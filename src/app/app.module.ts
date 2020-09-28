f895580b0b845b7ae2870d9ebfd51b4c8ee42e18
Alice Griffin <alicegriffin@Alices-MacBook-Air.local>
2020-09-28 18:51:23 -0400

    setup and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
